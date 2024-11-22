419d1e4702f32f802ef723377c412479d86c7d90
xybxy123 <2365421405@qq.com>
2024-11-23 01:56:22 +0800

    Initial version of hero_chassis_controller based on rm_template

---
+++ devel/_setup_util.py
@@ -0,0 +1,1 @@
+/home/xy/hero_chassis_controller/devel/.private/catkin_tools_prebuild/_setup_util.py
---
+++ devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
@@ -0,0 +1,1 @@
+/home/xy/hero_chassis_controller/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
